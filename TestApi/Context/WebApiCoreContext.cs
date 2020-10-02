c174a691a1d244e4ed73403504cfff37ddcfe014
dev.v.nazarenko@gmail.com <dev.v.nazarenko@gmail.com>
2020-10-02 16:24:24 +0300

    Add project files.

---
+++ TestApi/Context/WebApiCoreContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApi
+{
+    public class WebApiCoreContext : DbContext
+    {
+        // objects for bind with Db
+        public DbSet<CarClass> carClasses { get; set; }
+        public DbSet<CarVendor> carVendors { get; set; }
+        public DbSet<CarModel> carModels { get; set; }
+
+        public WebApiCoreContext(DbContextOptions<WebApiCoreContext> options) : base(options)
+        { 
+        
+        }
+        // methods for configure Db
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+        
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        { 
+        
+        }
+    }
+}
